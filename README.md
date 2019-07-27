33f022199a86e2b68106aa913cd3b4ab3c3672e3
Isaac Keleher <isaac.keleher@gmail.com>
2019-07-27 15:16:09 +1000

    New routes, pages and updated README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EduBox
 for RMIT ITS Hackathon 2019
+
+To run in dev mode with nodemon use
+```npm run dev```
+
+Navigate to http://localhost:2000/
--- app.js
+++ app.js
@@ -3,8 +3,31 @@ console.log("server started");
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-app.get('/',function(req, res) {//listens for when the client opens the site.
+
+//route for dashboard
+app.get('/',function(req, res) {
+    console.log("loading dashboard...");
     res.sendFile(__dirname + '/client/index.html');
+});
+//route for wikipedia
+app.get('/wiki',function(req, res) {
+    console.log("loading wikipedia...");
+    res.sendFile(__dirname + '/client/wiki.html');
+});
+//route for khan academy
+app.get('/khan_academy',function(req, res) {
+    console.log("loading khan academy...");
+    res.sendFile(__dirname + '/client/khan_academy.html');
+});
+//route for npm repo
+app.get('/npm_repo',function(req, res) {
+    console.log("loading npm repo...");
+    res.sendFile(__dirname + '/client/npm_repo.html');
+});
+//route for vr app
+app.get('/vr',function(req, res) {
+    console.log("loading VR resources...");
+    res.sendFile(__dirname + '/client/vr_app.html');
 });
 app.use('/client',express.static(__dirname + '/client'));//listens for when the client wants files.
 serv.listen(2000);
